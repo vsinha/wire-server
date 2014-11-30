724753de3444594ba4f5a09b06d4df6b91c0adee
Viraj Sinha <virajosinha@gmail.com>
2014-11-29 21:29:23 -0500

    logs what type of server we're running as

--- nodeServer/main.js
+++ nodeServer/main.js
@@ -2,7 +2,7 @@ var myFirebase = require('./myFirebase');
 var friendSync = require('./friendSync');
 var statusMentions = require('./statusMentions');
 
-console.log("Starting server as: ", process.argv[2] || 'dev');
+console.log("Starting server as:", process.argv[2] || 'dev');
 
 myFirebase.authAdmin(function () {
     friendSync.start();
